3309247d692039280dd978a497627241cb02fdd9
BLKNeko <69521435+BLKNeko@users.noreply.github.com>
2020-11-11 19:37:34 -0300

    Remove funcionando !

--- src/modules/finance/services/MovimentRemove.js
+++ src/modules/finance/services/MovimentRemove.js
@@ -1,16 +1,19 @@
 class MovimentRemove {
-    constructor(financeRepository) {
-      this.financeRepository = financeRepository;
-    }
-  
-    async execute(user_id, moviment_id) {
-      const moviment = await this.financeRepository.movimentRemove(moviment_id);
-  
-      if (moviment.user_id !== user_id) {
-        return { message: "you don't have permission to delete this moviment" };
-      }
-      return moviment;
-    }
+  constructor(financeRepository) {
+    this.financeRepository = financeRepository;
   }
-  
-  module.exports = MovimentRemove;
+
+  async execute(user_id, moviment_id) {
+    const movusid   =  await this.financeRepository.movimentById(moviment_id);
+    
+
+    if (movusid.user_id !== user_id) {
+      return { message: "you don't have permission to delete this moviment" };
+    }
+
+    const moviment = await this.financeRepository.movimentRemove(moviment_id);
+    return moviment;
+  }
+}
+
+module.exports = MovimentRemove;
